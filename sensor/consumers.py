9cff24cded69900646233244d3c96c1b6d6bcf38
saurabhkumar1989 <saurabhkumar.spsu@gmail.com>
2017-06-17 08:22:25 -0400

    initialization

---
+++ sensor/consumers.py
@@ -0,0 +1,22 @@
+from channels import Group
+
+def ws_connect(message):
+    print("Someone connected.")
+    path = message['path']
+    if path == '/sensor/':
+        print('Adding new user to sensor group')
+        Group('sensor').add(message.reply_channel)                             
+        message.reply_channel.send({                                            # Reply to individual directly
+           "text": "You're connected to sensor group :) ",
+        })
+    else:
+        print("Strange connector!!")
+
+def ws_message(message):
+    # ASGI WebSocket packet-received and send-packet message types
+    # both have a "text" key for their textual data.
+    print("Received!!" + message['text'])
+
+def ws_disconnect(message):
+    print("Someone left us...")
+    Group("sensor").discard(message.reply_channel)
